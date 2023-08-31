9c5344956eaa099061b6ac96c2abf79eaf922314
Sabari <r.sabarinathan02@gmail.com>
2023-08-31 12:18:10 +0530

    deploy

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // app.get('/', (req, res) => res.send('Api working!!'))
+app.get("/",(req,res) =>{
+    res.json("Hello World!")
+})
 
 //define routes
 app.use('/api/auth', require('./routes/api/auth'));
@@ -19,14 +22,18 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 //Serve static assests in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+// 	// Set static folder
+// 	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
+
+app.get('/',(req,res) =>{
+	res.send("Hello!");
+});
 
 const PORT = process.env.PORT || 5000;
 
