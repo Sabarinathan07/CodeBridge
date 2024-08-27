902f1a47174bae1a5d2a8fde0feab9a57e1e9420
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-08-28 01:44:17 +0530

    updated packages

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const db = process.env.MONGO_URI;
 const db_operations = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 };
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, db_operations);
-    console.log("MongoDB Connected.....");
-  } catch (err) {
-    console.error(err.message);
-    //exit the process with error message
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(db, db_operations);
+        console.log("MongoDB Connected.....");
+    } catch (err) {
+        console.error(err.message);
+        //exit the process with error message
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
