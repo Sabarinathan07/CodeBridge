de6cca088a04b588390459b9a9bc69d487d503d4
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-09-11 00:15:00 +0530

    Update Footer.js

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
-			<footer>
-				<div className='footer-content footer-container'>
-					<p>CodeBridge &copy; 2023</p>
-					{/* <p>We Unite! we Conquer!</p> */}
-				</div>
+        <div>
+            <footer>
+                <div className="footer-content footer-container">
+                    <p>CodeBridge &copy; 2024</p>
+                    {/* <p>We Unite! we Conquer!</p> */}
+                </div>
 
-				{/* <div className='footer-bottom'>
+                {/* <div className='footer-bottom'>
 					<p>
 						copyright &copy;2021 Sabari Blog 
 					</p>
 				</div> */}
-			</footer>
-		</div>
-	);
+            </footer>
+        </div>
+    );
 };
 
 export default Footer;
