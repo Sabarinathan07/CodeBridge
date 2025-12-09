21773fe93747f79bf7b0acb48c90f418cad96c14
Sabarinathan R <r.sabarinathan02@gmail.com>
2025-12-09 11:00:00 +0530

    feat: updated

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(cors());
 //     next();
 //   });
 
-// Middleware
+// Middleware is here
 app.use(express.json({ extended: false }));
 
 // app.get('/', (req, res) => res.send('Api working!!'))
