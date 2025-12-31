6657158016d843d225ce35e84c5755962fe079f9
Sabarinathan R <r.sabarinathan02@gmail.com>
2025-12-31 11:00:00 +0530

    feat: added changes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import userRoutes from "./routes/api/users.js";
 import postRoutes from "./routes/api/posts.js";
 import profileRoutes from "./routes/api/profile.js";
 
-// Initialize env her
+// Initialize env he
 dotenv.config();
 
 // Initialize express app
