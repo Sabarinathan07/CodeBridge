7fbbbcd50defdbadb4161860480e164f6b90702a
Sabarinathan R <r.sabarinathan02@gmail.com>
2026-02-10 11:00:00 +0530

    feat: changes

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -23,7 +23,7 @@ router.get("/", auth, async (req, res) => {
 // @route POST api/auth
 // @description authenticate user and get token
 // @access Public
-// validation with express-va
+// validation with express-v
 router.post(
     "/",
     [
